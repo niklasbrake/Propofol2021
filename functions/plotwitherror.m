40008c60ee9d8b85a799bb6dc34af08697ba3250
Niklas Brake <niklas.brake@mail.mcgill.ca>
2021-10-12 15:15:47 -0400

    Update plotwitherror.m

--- functions/plotwitherror.m
+++ functions/plotwitherror.m
@@ -1,16 +1,19 @@
 function h =plotwitherror(x,y,isstdrr,varargin)
 
-	if(isstdrr)
+	if(strcmp(isstdrr,'CI') || isstdrr == true)
 		ymu = nanmean(y,2);
 		% ymu = nanmedian(y,2);
+		z = icdf('norm',0.975,0,1);
+		ylo = ymu-z*stderror(y')';
+		yhi = ymu+z*stderror(y')';
+	elseif(strcmp(isstdrr,'SE') || isstdrr == false)
+		ymu = nanmean(y,2);
 		ylo = ymu-stderror(y')';
 		yhi = ymu+stderror(y')';
-	else
+	elseif(strcmp(isstdrr,'Q'))
 		ymu = nanmedian(y,2);
-		ylo = quantile(y,0.45,2);
-		% ylo = quantile(y,0.75,2);
-		yhi = quantile(y,0.55,2);
-		% yhi = quantile(y,0.25,2);
+		ylo = quantile(y,0.025,2);
+		yhi = quantile(y,0.925,2);
 	end
 
 	h = plot(x,ymu,varargin{:}); hold on;
@@ -24,6 +27,7 @@ function h =plotwitherror(x,y,isstdrr,varargin)
 	xfvec = [x,flip(x)];
 	yfvec = [ylo;flip(yhi)];
 	F = fill(xfvec(:),yfvec(:),'k');
+
 	if(isempty(F))
 		return;
 	end
